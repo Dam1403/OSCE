b7136c1c41624151502a3dabe2a2861d32993e59
Douglas May <Dam1403@rit.edu>
2019-07-24 23:51:28 -0400

    Saving Progress You were working on execvp

--- ch3/Project1/osh.c
+++ ch3/Project1/osh.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include "osh.h"
 
 #define MAX_LINE 80
 
@@ -17,17 +19,47 @@ int main(void)
     {
         printf("osh>");
         fflush(stdout);
-
+        
         
         fgets(buff,MAX_LINE,stdin);
+
         if(strnlen(buff,MAX_LINE) >= MAX_LINE - 1)
         {
             printf("Exceeded the maximum command length\n");
             return 0;    
         }
-        char* arg;
+
+        char* cmd_chunk = strtok(buff," ");
+        if(cmd_chunk[0] == '\n')
+        {
+            continue;
+        }
+        for(int i = 0; cmd_chunk != NULL; i++)
+        {
+            //printf("dsada");
+            printf("Arg %s\n",cmd_chunk);
+            args[i] = cmd_chunk;
+            cmd_chunk = strtok(NULL," ");
+        }
+        printf("Arg %s\n",cmd_chunk);
+        execute(args);
 
     }
 
     return 0;
+}
+
+
+void execute(char* cmd_chunks[])
+{
+    printf("Forking\n");
+    int pid = fork();
+    if(pid == 0)
+    {
+        printf("Exec Sucess %d\n",execvp(cmd_chunks[0],&cmd_chunks[1]));
+    }
+    else
+    {
+        wait(pid);
+    }
 }
---
+++ ch3/Project1/osh.h
@@ -0,0 +1,4 @@
+
+
+
+void execute(char* cmd_chunks[]);
